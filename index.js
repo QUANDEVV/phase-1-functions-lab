037987af1a245310f976193c308e70077e3865c1
QUANDEVV <vercelploy@gmail.com>
2023-03-21 13:42:07 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+let scuberHq = 42;
+function distanceFromHqInBlocks (distance) {
+
+  return Math.abs(distance - scuberHq)
+}
+
+
+function distanceFromHqInFeet (distance) {
+  return(Math.abs(distance-scuberHq) *264);
+}
+
+
+function distanceTravelledInFeet(start, destination) {
+  return Math.abs(destination-start)*264
+}
+
+function calculatesFarePrice(start, destination) {
+  const feet = Math.abs(destination-start) * 264
+  if (feet <= 400 ) 
+  {return 0}
+  else if (feet > 400 && feet <= 2000) {
+    return (feet -400 ) * 0.02
+  } else if (feet > 2000 && feet <= 2500) {
+    return 25
+
+  } else { 
+    return 'cannot travel that far'}
+}
